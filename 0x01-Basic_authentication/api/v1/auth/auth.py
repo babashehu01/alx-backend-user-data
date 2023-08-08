338b18bb6ae680c91f3619e9a6bd9523d33438ae
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-08-08 23:35:25 +0100

    Auth class update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
-""" Defines Auth class
+""" Module of auth
 """
 from flask import request
+from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class
-    """
-    def __init__(self):
-        """
-            Constructor
-
-            Args:
-                path: path to authenticate
-                excluded_paths: list of excluded path to authenticate
-        """
+    """ Auth Class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
@@ -27,7 +19,20 @@ class Auth:
             Return:
                 True if is authenticated otherwise false
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] is not '/':
+            path += '/'
+
+        for paths in excluded_paths:
+            if paths.endswith('*'):
+                if path.startswith(paths[:-1]):
+                    return False
+            elif path == paths:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
@@ -39,7 +44,10 @@ class Auth:
             Return:
                 The authorization header or None
         """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
@@ -51,4 +59,4 @@ class Auth:
             Return:
                 The user
         """
-        return None
+        return request
