09c38f20384c76ddff7a90095f6640bdc16a74c0
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-08-08 23:30:54 +0100

    Adding Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Defines Auth class
+"""
+from flask import request
+
+class Auth:
+    """Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+            Require the auth
+
+            Args:
+                path: path to authenticate
+                excluded_paths: list of excluded path to authenticate
+
+            Return:
+                True if is authenticated otherwise false
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+            Look the headers
+
+            Args:
+                request: Look the autthorization
+
+            Return:
+                The authorization header or None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+            Look current user
+
+            Args:
+                request: Look the reques user
+
+            Return:
+                The user
+        """
+        return None
